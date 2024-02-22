e299cdda44e6433bea808b87046d41a01d5ddb5c
adidas933 <adidas933@hotmail.com>
2024-02-22 13:57:44 +0200

    commit3

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,14 @@ function getEpisodes(API:string) {
         { data: "name", title: "Name" },
         { data: "air_date", title: "Air Date" },
         { data: "episode", title: "Episode" },
-        { data: "url", title: "URL" },
-        { data: "created", title: "Created" }
+        // { data: "url", title: "URL" },
+        // { data: "created", title: "Created" }
       ]
     })
-    const onlyAbove12 = data.results.filter((episode:Episode) => {
+    const onlyAbove12 = episodesData.filter((episode:Episode) => {
       return episode.characters.length > 12;
     })
+    
     console.log(onlyAbove12);
     $('.above12Table').DataTable({
       data: onlyAbove12,
@@ -38,24 +39,49 @@ function getEpisodes(API:string) {
         { data: "name", title: "Name" },
         { data: "air_date", title: "Air Date" },
         { data: "episode", title: "Episode" },
-        { data: "url", title: "URL" },
-        { data: "created", title: "Created" },
+        // { data: "url", title: "URL" },
+        // { data: "created", title: "Created" },
       ]
     })
     onlyAbove12.forEach((episode:Episode, index:number) => {
-      const ol = $(`<ol class="episodeName"><strong>${index + 1}</strong> ${episode.name}:</ol>`);
+      const ol = $(`<ol class="episodesOl"><p class="episodeName">${index + 1}. Episode "${episode.name}":<p/></ol>`);
       for (let i = 0; i < episode.characters.length; i++) {
         $.ajax({
           url: (episode.characters)[i],
           dataType: 'json',
           success: function (data) {
-            const ul = document.createElement('ul');
+            const checkBoxLine = `  
+            <div>
+              <label>Dead
+                <input id="dead${data.id}input" type="checkbox">
+              </label>
+              <label>Alive
+                <input id="alive${data.id}input" type="checkbox">
+              </label>
+              <label>Unknown
+                <input id="unknown${data.id}input" type="checkbox">
+              </label>
+            </div>`;
+          /*   if (data.status === 'Dead') {
+              $(`#dead${data.id}input`).prop('checked', true);
+              $(`#alive${data.id}input`).prop('checked', false);
+              $(`#unknown${data.id}input`).prop('checked', false);
+              console.log(`${data.status} ${data.id}`);
+            } else if (data.status === 'Alive') {
+              $(`#dead${data.id}input`).prop('checked', false);
+              $(`#alive${data.id}input`).prop('checked', true);
+              $(`#unknown${data.id}input`).prop('checked', false);
+              console.log(`${data.status} ${data.id}`);
+            } else if (data.status === 'Unknown'){
+              $(`#dead${data.id}input`).prop('checked', false);
+              $(`#alive${data.id}input`).prop('checked', false);
+              $(`#unknown${data.id}input`).prop('checked', true);
+              console.log(`${data.status} ${data.id}`);
+            } */
+            const ul = $('<ul>');
             ol.append(ul);
-            $(ul).append(`<li><strong>${i + 1} </strong>${data.name}</li>`);
-            $(ul).append(`<img src="${data.image}"></img>`);
-            $(ul).append(`<li>status: ${data.status}</li>`);
-            $(ul).append(`<li>Species: ${data.species}</li>`);
-            $(ul).append(`<li>Gender: ${data.gender}</li>`);
+            const charImg = (`<img class="characterImg" src="${data.image}">`);
+            ul.append(`<li><strong>${i + 1} </strong> ${charImg} ${data.name} ${data.id}</li>`);
           }
         })
       }
